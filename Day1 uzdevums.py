ee10ac0d5eb97c0ae2879670d9c7c3c77c47a380
WeirdoKT <86868192+WeirdoKT@users.noreply.github.com>
2023-06-06 20:47:52 +0300

    Update Day1 uzdevums.py

--- Day1 uzdevums.py
+++ Day1 uzdevums.py
@@ -1,10 +1,9 @@
 i = 0
 my_name = "Kalvis"
-while i != 6:
-    if i != 0:
-        print(f"{my_name} #{i}")
-        i += 1
-    else: i +=1
+while i < 5:
+
+    print(f"{my_name} #{i + 1}")
+    i += 1
 
 my_name_x_times = my_name * 6
 print(my_name_x_times) # dubplicates and concatenates string the amount of times it's multiplied
